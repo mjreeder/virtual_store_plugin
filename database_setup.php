e9628c22ca56e6833d90ce23762bfb25a2b191fb
Bryon O'Conner <bpoconner@bsu.edu>
2017-01-20 12:06:09 -0500

    Added initial Database into plugin

---
+++ database_setup.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * This file is used to setup the database and database changes between versions.
+ * Here is the wordpress documentation regarding this
+ * https://codex.wordpress.org/Creating_Tables_with_Plugins
+ *
+ * here is some additional info on how to use wpdb
+ * https://codex.wordpress.org/Class_Reference/wpdb
+ */
+defined( 'ABSPATH' ) or die( 'invalid access' );
+define("DCVS_DATABASE_VERSION", 0.1);
+
+$dcvs_current_version = dcvs_get_option("dcvs_database_version");
+
+if($dcvs_current_version != DCVS_DATABASE_VERSION){
+    require_once __DIR__."/../../../wp-admin/includes/upgrade.php";
+    $charset_collate = $wpdb->get_charset_collate();
+    if($dcvs_current_version < 0.1){
+        $personaTable = "CREATE TABLE dcvs_persona(
+            id BIGINT(10) NOT NULL AUTO_INCREMENT,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            money DOUBLE(8,2) DEFAULT 0 NOT NULL,
+            PRIMARY KEY  (id)
+            ) $charset_collate;";
+        dbDelta($personaTable);
+
+        $userPersonaTable = "CREATE TABLE dcvs_user_persona(
+          id BIGINT(10) NOT NULL AUTO_INCREMENT,
+          user_id BIGINT(10) NOT NULL,
+          persona_id BIGINT(10),
+          PRIMARY KEY  (id)
+        ) $charset_collate;";
+        dbDelta($userPersonaTable);
+
+        $purchaseTable = "CREATE TABLE dcvs_purchase(
+            id BIGINT(10) NOT NULL AUTO_INCREMENT,
+            user_persona_id BIGINT(10) NOT NULL,
+            business_id BIGINT(10) NOT NULL,
+            items TEXT NOT NULL,
+            cost DOUBLE(8,2) NOT NULL,
+            PRIMARY KEY  (id)
+        ) $charset_collate;";
+
+        $currentPersonaTable = "CREATE TABLE dcvs_current_persona(
+            user_id BIGINT(10) NOT NULL,
+            persona_id BIGINT(10)
+        ) $charset_collate;";
+
+        dbDelta($currentPersonaTable);
+
+        $businessTable = "CREATE TABLE dcvs_business(
+          id BIGINT(10) NOT NULL AUTO_INCREMENT,
+          title VARCHAR(400) NOT NULL,
+          description TEXT NOT NULL,
+          money DOUBLE(8,2) NOT NULL,
+          url VARCHAR(500) NOT NULL,
+          PRIMARY KEY  (id)
+        ) $charset_collate;";
+
+        dbDelta($businessTable);
+
+        $userBusinessTable = "CREATE TABLE dcvs_user_business(
+            user_id BIGINT(10) NOT NULL,
+            business_id BIGINT(10) NOT NULL
+        ) $charset_collate;";
+        dbDelta($userBusinessTable);
+
+        $warehousePurchaseTable = "CREATE TABLE dcvs_warehouse_purchase(
+            id BIGINT(10) NOT NULL AUTO_INCREMENT,
+            user_id BIGINT(10) NOT NULL,
+            cost DOUBLE(8,2) NOT NULL,
+            items TEXT NOT NULL,
+            PRIMARY KEY  (id)
+        ) $charset_collate;";
+        dbDelta($warehousePurchaseTable);
+
+        $optionsTable = "CREATE TABLE dcvs_options(
+            id BIGINT(10) NOT NULL AUTO_INCREMENT,
+            option_key VARCHAR(200) NOT NULL,
+            option_value LONGTEXT NOT NULL,
+            PRIMARY KEY  (id)
+        ) $charset_collate;";
+        dbDelta($optionsTable);
+
+    }
+
+    dcvs_set_option("dcvs_database_version", DCVS_DATABASE_VERSION);
+}
+
--- plugin.php
+++ plugin.php
@@ -1,26 +1,33 @@
 <?php
 /**
- * Plugin Name: Digital Corps Woocommerce Exporter
- * Description: Used to export a Woocommerce cart
+ * Plugin Name: Digital Corps Virtual Store Plugin
+ * Description: Plugin used for class
  * Version: 0.1
  * Author: Digital Corps, Ball State University
  */
 defined( 'ABSPATH' ) or die( 'invalid access' );
 
-add_action("woocommerce_review_order_before_payment", "dc_before_cart_contents");
-add_action("woocommerce_review_order_after_payment", "dc_after_cart_contents");
-add_action("init", "dc_plugin_init");
+add_action("woocommerce_review_order_before_payment", "dcvs_before_cart_contents");
+add_action("woocommerce_review_order_after_payment", "dcvs_after_cart_contents");
+add_action("init", "dcvs_plugin_init");
 
-function dc_plugin_init(){
+
+register_activation_hook( __FILE__, "dcvs_activation_plugin" );
+function dcvs_activation_plugin(){
+    include "database_setup.php";
+}
+
+function dcvs_plugin_init(){
     //if the user is not logged in redirect them to do so
     if ( !is_user_logged_in() && $GLOBALS['pagenow'] !== 'wp-login.php'  ) {
         auth_redirect();
         exit;
     }
+    include "database_setup.php";
 }
 
 
-function dc_before_cart_contents(){
+function dcvs_before_cart_contents(){
     ?>
     <div class="cart-export">
         <?php
@@ -39,10 +46,26 @@ function dc_before_cart_contents(){
 }
 
 //This function is used to close the hidden div that hides the payment processing section of checkout
-function dc_after_cart_contents(){
+function dcvs_after_cart_contents(){
     ?>
         </div>
     <?php
+}
+
+function dcvs_get_option($key, $default_value=false){
+    global $wpdb;
+
+    $result = $wpdb->get_var("SELECT option_value FROM dcvs_options WHERE option_key='".esc_sql($key)."'");
+    return $result == NULL ? $default_value : $result;
+}
+
+function dcvs_set_option($key, $value){
+    global $wpdb;
+    if(dcvs_get_option($key)!= NULL){
+        $wpdb->update("dcvs_options", array("option_value"=>$value), array("option_key"=>$key));
+    }else{
+        $wpdb->insert("dcvs_options", ["option_key"=>$key, "option_value"=>$value] );
+    }
 }
 
 ?>
