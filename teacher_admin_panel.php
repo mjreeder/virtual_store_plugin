2daad95fa95468bc2b28e3d889e6dcb93f3dd169
Matt Reeder <mjreeder@bsu.edu>
2017-02-02 14:06:09 -0500

    pull student information out to into template

--- teacher_admin_panel.php
+++ teacher_admin_panel.php
@@ -30,6 +30,7 @@ function dcvs_admin_menu_draw()
         dcvs_set_option('default_persona_money', $defaultPersonaMoney);
     }
     ?>
+    <!-- template -->
     <form action="" method="post">
         <input type="hidden" name="dcvs_admin_changes" value="1">
         <label>Default Persona Money</label> <input name="default_persona_money" type="text" value="<?php dcvs_echo_option('default_persona_money', 0);
@@ -59,27 +60,5 @@ function dcvs_admin_businesses_settings()
 
 function dvcs_admin_teacher_settings()
 {
-    ?>
-    <h1>Teacher Admin</h1>
-    <?php
-    global $wpdb;
-    $results = $wpdb->get_results('SELECT * FROM dcvs_user_business', OBJECT);
-    for ($i = 0; $i < sizeof($results); ++$i) {
-        $display_name = $wpdb->get_results($wpdb->prepare('SELECT display_name FROM wp_users WHERE id = %d', $results[$i]->user_id));
-        $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $results[$i]->business_id));
-        $personas = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_persona LEFT JOIN dcvs_user_persona ON dcvs_persona.id=dcvs_user_persona.id WHERE user_id = %d', $results[$i]->user_id));
-        ?>
-      <div>
-        <label name="student_name" type="text"><?php echo $display_name[0]->display_name;
-        ?></label>
-          <div><?php echo $business[0]->title;
-        ?></div>
-        <div><?php echo $personas[0]->name;
-        ?></div>
-      <div><?php echo $personas[1]->name;
-        ?></div>
-      </div>
-        <?php
-
-    }
+  require_once __DIR__."/templates/teacher_admin_home.php";
 }
