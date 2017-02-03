57cc0f551b190db1764bb26b96a713b33df19196
Rachel Harvey <raharvey@bsu.edu>
2017-02-03 13:33:21 -0500

    got update and insert working going to separate the logic next

--- teacher_admin_panel.php
+++ teacher_admin_panel.php
@@ -35,9 +35,35 @@ function dcvs_admin_menu_draw(){
 }
 
 function dcvs_admin_personas_settings(){
+  if($_SERVER['REQUEST_METHOD']=="POST" && $_POST['dcvs_admin_changes']==1){
+    $name = $_POST['persona_name'];
+    $description = $_POST['persona_description'];
+    $money = $_POST['persona_money'];
+    $id = dcvs_get_persona($name);
+    global $wpdb;
+    if ($id != NULL) {
+        $wpdb->update("dcvs_persona", array("name"=>$name, "description"=>$description,"money"
+      =>$money), array("id"=>$id));
+    } else {
+        $wpdb->insert("dcvs_persona", ["name"=>$name, "description"=>$description] );
+    }
+  }
     ?>
-    yo dawg
+    <form action="" method="post">
+        <input type="hidden" name="dcvs_admin_changes" value="1">
+        <label>Persona</label><input name="persona_name" type="text" value="<?php dcvs_echo_option("persona_name","Name"); ?>">
+        <label></label><input name="persona_description" type="text" value="<?php dcvs_echo_option("persona_description","Description"); ?>">
+        <label></label><input name="persona_money" type="text" value="<?php dcvs_echo_option("persona_decription", "Money"); ?>">
+        <input type="submit">
+    </form>
     <?php
+}
+
+function dcvs_get_persona($name, $default_value=false){
+    global $wpdb;
+
+    $result = $wpdb->get_var("SELECT id FROM dcvs_persona WHERE name='".esc_sql($name)."'");
+    return $result == NULL ? $default_value : $result;
 }
 
 function dcvs_admin_businesses_settings(){
@@ -45,4 +71,3 @@ function dcvs_admin_businesses_settings(){
     INSERT WITTY SAYING
     <?php
 }
-
