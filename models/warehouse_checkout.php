8673c0930325770564815c00b7c058e12cef7986
Richard Downey <rdowney117@gmail.com>
2017-02-08 11:30:53 -0500

    Add skeleton methods to warehouse checkout class

--- models/warehouse_checkout.php
+++ models/warehouse_checkout.php
@@ -1,6 +1,7 @@
 <?php
 
 if (!class_exists('WarehouseCheckout')) {
+
     class WarehouseCheckout {
 
         public function __construct() {
@@ -8,10 +9,29 @@ if (!class_exists('WarehouseCheckout')) {
         }
 
         public function init() {
-
+	        add_action( "woocommerce_checkout_order_processed", array( $this, "dcvs_export_cart" ) );
         }
+
+	    function dcvs_export_cart($order_id) {
+		    self::dcvs_export_attributes();
+		    self::dcvs_export_variations();
+		    self::dcvs_export_products();
+	    }
+
+	    function dcvs_export_attributes() {
+
+	    }
+
+	    function dcvs_export_variations() {
+		    
+	    }
+
+	    function dcvs_export_products() {
+
+	    }
+
     }
-	
+
 	new WarehouseCheckout();
 
 }
