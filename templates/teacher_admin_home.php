5066d601038c08a07efd48a48d9d0f7418e0fd14
Matt Reeder <mjreeder@bsu.edu>
2017-02-09 14:35:46 -0500

    Add form for editing the end of shopping time for stores

--- templates/teacher_admin_home.php
+++ templates/teacher_admin_home.php
@@ -1,33 +1,9 @@
 
 <h1>Teacher Admin</h1>
 <?php
-global $wpdb;
-$results = $wpdb->get_results('SELECT * FROM dcvs_user_business', OBJECT);
-for ($i = 0; $i < sizeof($results); ++$i) {
-    $display_name = $wpdb->get_results($wpdb->prepare('SELECT display_name FROM wp_users WHERE id = %d', $results[$i]->user_id));
-    $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $results[$i]->business_id));
-    $personas = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_persona LEFT JOIN dcvs_user_persona ON dcvs_persona.id=dcvs_user_persona.id WHERE user_id = %d', $results[$i]->user_id));
-    ?>
-  <div>
-    <label name="student_name" type="text"><?php echo $display_name[0]->display_name;
-    ?></label>
-      <div><?php echo $business[0]->url;
-    ?></div>
-    <div><?php echo $personas[0]->name;
-    ?></div>
-  <div><?php echo $personas[1]->name;
-    ?></div>
-    <?php var_dump(get_persona_cart($results[$i]->user_id));
-      ?></div>
-  </div>
 
-    <?php
-
-}
-?>
-change end shopping time<input type='text'/>
-<button>update</button>
-<?php
+display_some_random_user_data();
+get_shopping_time_remaining();
 
 
 function get_store_info($business_id){
@@ -39,14 +15,57 @@ function get_store_info($business_id){
 
 }
 
-function get_persona_cart($user_id){
+function display_some_random_user_data(){
   global $wpdb;
-  $user_cart_meta_data = $business = $wpdb->get_results($wpdb->prepare("SELECT meta_value FROM wp_usermeta WHERE user_id = %d AND meta_key = '_woocommerce_persistent_cart'", $user_id));
-  return $user_cart_meta_data;
+  $results = $wpdb->get_results('SELECT * FROM dcvs_user_business', OBJECT);
+  for ($i = 0; $i < sizeof($results); ++$i) {
+      $display_name = $wpdb->get_results($wpdb->prepare('SELECT display_name FROM wp_users WHERE id = %d', $results[$i]->user_id));
+      $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $results[$i]->business_id));
+      $personas = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_persona LEFT JOIN dcvs_user_persona ON dcvs_persona.id=dcvs_user_persona.id WHERE user_id = %d', $results[$i]->user_id));
+      ?>
+    <div>
+      <label name="student_name" type="text"><?php echo $display_name[0]->display_name;
+      ?></label>
+        <div><?php echo $business[0]->url;
+      ?></div>
+      <div><?php echo $personas[0]->name;
+      ?></div>
+    <div><?php echo $personas[1]->name;
+      ?></div>
+      <?php var_dump(get_user_persona_order_history($results[$i]->user_id, $personas[0]->id));
+        ?></div>
+      <?php var_dump(get_user_persona_order_history($results[$i]->user_id, $personas[1]->id));
+        ?></div>
+    </div>
+      <?php
+  }
 }
 
-function get_time_shopping_time_remaining(){
+function get_user_persona_order_history($user_id, $persona_id){
   global $wpdb;
-  $result = $wpdb->get_var("SELECT option_value FROM dcvs_options WHERE option_key='shopping_end_date'");
-  var_dump($result);
+  $user_persona_order_history = $wpdb->get_results($wpdb->prepare("SELECT * FROM dvcs_business_purchase WHERE user_id = %d AND persona_id = %d", $user_id, $persona_id));
+  return $user_persona_order_history;
+}
+
+function get_shopping_time_remaining(){
+  if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['dcvs_admin_changes'] == 1) {
+      $shopping_end_date = $_POST['shopping_end_date'];
+      if (!DateTime::createFromFormat('Y-m-d', $shopping_end_date)) {
+          echo 'shopping end date must be a date in the format of yyyy-mm-dd';
+
+          return;
+      }
+      dcvs_set_option('shopping_end_date', $shopping_end_date);
+  }
+  ?>
+  <!-- template -->
+  <form action="" method="post">
+      <input type="hidden" name="dcvs_admin_changes" value="1">
+      <label>Shopping end date</label> <input name="shopping_end_date" type="text" value="<?php dcvs_echo_option('shopping_end_date', 0);
+  ?>">
+      <input type="submit">
+
+  </form>
+  <?php
+
 }
