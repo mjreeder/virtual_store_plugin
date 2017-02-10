b1843e63d081ab616faeb5c58607d8b099300234
Matt Reeder <mjreeder@bsu.edu>
2017-02-10 14:03:08 -0500

    add input box for warehouse end date

--- templates/teacher_admin_home.php
+++ templates/teacher_admin_home.php
@@ -3,66 +3,95 @@
 <?php
 
 display_some_random_user_data();
-get_shopping_time_remaining();
+get_shopping_end_date();
+get_warehouse_end_date();
 
-
-function get_store_info($business_id){
-  global $wpdb;
-  $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $business_id));
-  $current_money = $business->money;
-  $description = $business->description;
-  $display_name = $business->display_name;
-
+function get_store_info($business_id)
+{
+    global $wpdb;
+    $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $business_id));
+    $current_money = $business->money;
+    $description = $business->description;
+    $display_name = $business->display_name;
 }
 
-function display_some_random_user_data(){
-  global $wpdb;
-  $results = $wpdb->get_results('SELECT * FROM dcvs_user_business', OBJECT);
-  for ($i = 0; $i < sizeof($results); ++$i) {
-      $display_name = $wpdb->get_results($wpdb->prepare('SELECT display_name FROM wp_users WHERE id = %d', $results[$i]->user_id));
-      $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $results[$i]->business_id));
-      $personas = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_persona LEFT JOIN dcvs_user_persona ON dcvs_persona.id=dcvs_user_persona.id WHERE user_id = %d', $results[$i]->user_id));
-      ?>
+function display_some_random_user_data()
+{
+    global $wpdb;
+    $results = $wpdb->get_results('SELECT * FROM dcvs_user_business', OBJECT);
+    for ($i = 0; $i < sizeof($results); ++$i) {
+        $display_name = $wpdb->get_results($wpdb->prepare('SELECT display_name FROM wp_users WHERE id = %d', $results[$i]->user_id));
+        $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $results[$i]->business_id));
+        $personas = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_persona LEFT JOIN dcvs_user_persona ON dcvs_persona.id=dcvs_user_persona.id WHERE user_id = %d', $results[$i]->user_id));
+        ?>
     <div>
       <label name="student_name" type="text"><?php echo $display_name[0]->display_name;
-      ?></label>
+        ?></label>
         <div><?php echo $business[0]->url;
-      ?></div>
+        ?></div>
       <div><?php echo $personas[0]->name;
-      ?></div>
+        ?></div>
     <div><?php echo $personas[1]->name;
-      ?></div>
-      <?php var_dump(get_user_persona_order_history($results[$i]->user_id, $personas[0]->id));
+        ?></div>
+      <!-- <?php var_dump(get_user_persona_order_history($results[$i]->user_id, $personas[0]->id));
         ?></div>
       <?php var_dump(get_user_persona_order_history($results[$i]->user_id, $personas[1]->id));
-        ?></div>
+        ?></div> -->
     </div>
       <?php
-  }
+
+    }
 }
 
-function get_user_persona_order_history($user_id, $persona_id){
-  global $wpdb;
-  $user_persona_order_history = $wpdb->get_results($wpdb->prepare("SELECT * FROM dvcs_business_purchase WHERE user_id = %d AND persona_id = %d", $user_id, $persona_id));
-  return $user_persona_order_history;
+function get_user_persona_order_history($user_id, $persona_id)
+{
+    global $wpdb;
+    $user_persona_order_history = $wpdb->get_results($wpdb->prepare('SELECT * FROM dvcs_business_purchase WHERE user_id = %d AND persona_id = %d', $user_id, $persona_id));
+
+    return $user_persona_order_history;
 }
 
-function get_shopping_time_remaining(){
-  if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['dcvs_admin_changes'] == 1) {
-      $shopping_end_date = $_POST['shopping_end_date'];
-      if (!DateTime::createFromFormat('Y-m-d', $shopping_end_date)) {
-          echo 'shopping end date must be a date in the format of yyyy-mm-dd';
+function get_shopping_end_date()
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['dcvs_admin_changes'] == 1 && isset($_POST['shopping_end_date'])) {
+        $shopping_end_date = $_POST['shopping_end_date'];
+        if (!DateTime::createFromFormat('Y-m-d', $shopping_end_date)) {
+            echo 'shopping end date must be a date in the format of yyyy-mm-dd';
 
-          return;
-      }
-      dcvs_set_option('shopping_end_date', $shopping_end_date);
-  }
-  ?>
+            return;
+        }
+        dcvs_set_option('shopping_end_date', $shopping_end_date);
+    }
+    ?>
   <!-- template -->
   <form action="" method="post">
       <input type="hidden" name="dcvs_admin_changes" value="1">
       <label>Shopping end date</label> <input name="shopping_end_date" type="text" value="<?php dcvs_echo_option('shopping_end_date', 0);
-  ?>">
+    ?>">
+      <input type="submit">
+
+  </form>
+  <?php
+
+}
+
+function get_warehouse_end_date()
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['dcvs_admin_changes'] == 1 && isset($_POST['warehouse_end_date'])) {
+        $warehouse_end_date = $_POST['warehouse_end_date'];
+        if (!DateTime::createFromFormat('Y-m-d', $warehouse_end_date)) {
+            echo 'warehouse end date must be a date in the format of yyyy-mm-dd';
+
+            return;
+        }
+        dcvs_set_option('warehouse_end_date', $warehouse_end_date);
+    }
+    ?>
+  <!-- template -->
+  <form action="" method="post">
+      <input type="hidden" name="dcvs_admin_changes" value="1">
+      <label>warehouse end date</label> <input name="warehouse_end_date" type="text" value="<?php dcvs_echo_option('warehouse_end_date', 0);
+    ?>">
       <input type="submit">
 
   </form>
