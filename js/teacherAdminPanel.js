5a138b887723127e769523d6c16ea7fcfc8de90e
Matt Reeder <mjreeder@bsu.edu>
2017-02-20 14:13:59 -0500

    Add search capability and fix the selected student css class

---
+++ js/teacherAdminPanel.js
@@ -0,0 +1,44 @@
+
+setTimeout(function(){
+  var id = getParameterByName('student_id');
+  var d = document.getElementById(id);
+  console.log(d);
+  d.className += "selectedStudent";
+}, 150);
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function studentSearch() {
+  var studentElements = document.getElementById('students');
+  var searchQuery = $("#search").val();
+  var possible = [];
+  for (var i = 0; i < studentElements.children.length; i++) {
+    if (studentElements.children[i] !== undefined) {
+      var sliced = studentElements.children[i].innerText.slice(0, searchQuery.length);
+      if (sliced.toLowerCase() == searchQuery.toLowerCase()) {
+          possible.push(i);
+      }
+    }
+  }
+
+  for (var i = 0; i < studentElements.children.length; i++) {
+    if (studentElements.children[i] !== undefined) {
+      if(possible.indexOf(i) == -1){
+        studentElements.children[i].hidden = true;
+      }
+      else{
+        studentElements.children[i].hidden = false;
+      }
+    }
+  }
+}
--- templates/teacher_admin_home.php
+++ templates/teacher_admin_home.php
@@ -1,9 +1,9 @@
-
-<h1>Teacher Admin</h1>
 <?php
-get_shopping_end_date();
-get_warehouse_end_date();
-display_some_random_user_data();
+
+// get_shopping_end_date();
+// get_warehouse_end_date();
+display_admin_panel();
+// display_some_random_user_data();
 
 function get_store_info($business_id)
 {
@@ -13,53 +13,142 @@ function get_store_info($business_id)
     $description = $business->description;
     $display_name = $business->display_name;
 }
-
-function display_some_random_user_data()
+function display_admin_panel()
 {
-    $user_results;
-    $display_names = array();
-    global $wpdb;
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['dcvs_admin_changes'] == 1 && isset($_POST['student_name'])) {
-        $student_name = $_POST['student_name'];
-        var_dump($student_name);
-        var_dump($display_names);
-    }
     ?>
-  <!-- template -->
-  <form action="" method="post">
-      <input type="hidden" name="dcvs_admin_changes" value="1">
-      <input name="student_name" placeholder="name" type="text">
-      <input type="submit">
-  </form>
+  <head>
+      <title>Virtual Store Admin Panel</title>
+      <!-- CSS -->
+      <link href="<?php echo 'wp-content/plugins/virtual_store_plugin/assets/css/adminPanel.css';
+    ?>" rel="stylesheet" type="text/css">
+      <!-- FONTS -->
+      <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+      <script src="<?php echo 'wp-content/plugins/virtual_store_plugin/js/teacherAdminPanel.js';
+    ?>" rel="stylesheet"></script>
+
+      <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Open+Sans:400,600,700" rel="stylesheet">
+  </head>
+  <body>
+
+      <header class="header">
+
+          <h1>virtual store admin panel</h1>
+
+      </header>
+
+      <main class="admin">
+
+          <nav class="sidebar">
+              <ul>
+                  <li id="selectedTab">STUDENT INFO</li>
+                  <a href="../views/merchandiser.html">
+                      <li>MERCHANDISER</li>
+                  </a>
+                  <a href="../views/assign.html">
+                      <li>ASSIGN PERSONAS</li>
+                  </a>
+                  <a href="../views/manage.html">
+                      <li>MANAGE PERSONAS</li>
+                  </a>
+                  <a href="../views/settings.html">
+                      <li>GENERAL SETTINGS</li>
+                  </a>
+              </ul>
+          </nav>
+          <aside class="studentList">
+              <div class="searchBar">
+                  <img src="<?php echo 'wp-content/plugins/virtual_store_plugin/assets/images/search.svg';
+    ?>" rel="stylesheet" alt="">
+                  <input type="text" id='search' placeholder="search" oninput="studentSearch()">
+              </div>
+              <ul id="students">
+              <?php
+                display_student_list();
+    ?>          </ul>
+          </aside>
+          <?php
+            display_current_student_info();
+    ?>
+      </main>
+
+  </body>
 
   <?php
+
+}
+function display_student_list()
+{
+    global $wpdb;
     $user_results = $wpdb->get_results('SELECT * FROM dcvs_user_business', OBJECT);
     ?>
     <?php
     for ($i = 0; $i < sizeof($user_results); ++$i) {
         $display_name = $wpdb->get_results($wpdb->prepare('SELECT display_name FROM wp_users WHERE id = %d', $user_results[$i]->user_id));
-        // array_push($display_names, $display_name[0]->display_name);
-        $display_names[] = $display_name[0]->display_name;
         $business = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business WHERE id = %d', $user_results[$i]->business_id));
         $personas = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_persona LEFT JOIN dcvs_user_persona ON dcvs_persona.id=dcvs_user_persona.id WHERE user_id = %d', $user_results[$i]->user_id));
         ?>
-    <div>
-      <label name="student_name" type="text"><?php echo $display_name[0]->display_name;
-        ?></label>
-        <div><?php echo $business[0]->url;
-        ?></div>
-      <div><?php echo $personas[0]->name;
-        ?></div>
-    <div><?php echo $personas[1]->name;
-        ?></div>
-      <?php get_user_persona_order_history($user_results[$i]->user_id, $personas[0]->id);
-        ?></div>
-      <?php get_user_persona_order_history($user_results[$i]->user_id, $personas[1]->id);
-        ?></div>
-    </div>
+
+
+      <li id="<?php echo $user_results[$i]->user_id;
+        ?>" name="student_name" type="text"><a href="<?php echo $_SERVER['REQUEST_URI'].'&student_id='.$user_results[$i]->user_id;
+        ?>"><?php echo $display_name[0]->display_name;
+        ?></a></li>
+
       <?php
 
     }
+}
+
+function display_current_student_info()
+{
+    global $wpdb;
+    $currentDisplayStudent = $_REQUEST['student_id'];
+    $display_name = $wpdb->get_results($wpdb->prepare('SELECT display_name FROM wp_users WHERE id = %d', $currentDisplayStudent));
+    $business_info = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_business LEFT JOIN dcvs_user_business ON dcvs_business.id=dcvs_user_business.business_id WHERE user_id = %d', $currentDisplayStudent));
+    $persona_info = $wpdb->get_results($wpdb->prepare('SELECT * FROM dcvs_persona LEFT JOIN dcvs_user_persona ON dcvs_persona.id=dcvs_user_persona.persona_id WHERE user_id = %d', $currentDisplayStudent));
+    ?>
+
+  <section class="studentInfo">
+      <h1><?php echo $display_name[0]->display_name ?></h1>
+      <section class="merchandiserInfo">
+          <h2><?php echo $business_info[0]->title ?></h2>
+          <p><strong>you are:</strong> <?php echo $business_info[0]->description ?></p>
+          <div>
+              <a href="<?php echo $business_info[0]->url ?>" class="button">Personal Site</a>
+              <button class="button">COMPARISON</button>
+              <button class="button">FINAL SURVEY</button>
+          </div>
+
+      </section>
+      <section class="shopperInfo">
+          <h2>shopper</h2>
+          <aside class="shopperOne">
+              <div>
+                  <h3><?php echo $persona_info[0]->name ?></h3>
+                  <img src="<?php echo 'wp-content/plugins/virtual_store_plugin/assets/images/personaRed.png';
+    ?>" alt="">
+              </div>
+              <p><strong>you are:</strong> <?php echo $persona_info[0]->description ?></p>
+              <button class="button buttonSmall">ORDER HISTORY</button>
+              <button class="button buttonSmall">SURVEY</button>
+          </aside>
+          <aside class="shopperTwo">
+              <div>
+                  <h3><?php echo $persona_info[1]->name ?></h3>
+                  <img src="<?php echo 'wp-content/plugins/virtual_store_plugin/assets/images/personaBlue.png';
+    ?>" alt="">
+              </div>
+              <p><strong>you are:</strong> <?php echo $persona_info[1]->description ?></p>
+              <button class="button buttonSmall">ORDER HISTORY</button>
+              <button class="button buttonSmall">SURVEY</button>
+          </aside>
+      </section>
+
+  </section>
+  <?php
+
 }
 
 function get_user_persona_order_history($user_id, $persona_id)
@@ -92,10 +181,6 @@ function get_shopping_end_date()
   </form>
   <?php
 
-}
-
-function filter_user_list()
-{
 }
 
 function get_warehouse_end_date()
