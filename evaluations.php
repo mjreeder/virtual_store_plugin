22f036e98cf8f32456dbcb229437e029fa0c2718
Riley Paulsen <rileypaulsen.bsu@gmail.com>
2017-04-03 07:42:34 -0400

    Handles most of the basics for evaluations, including: * form field population * redirection pseudocode * base logic for determining which form to use

---
+++ evaluations.php
@@ -0,0 +1,22 @@
+<?php
+
+add_action('woocommerce_thankyou','dcvs_survey_time');
+function dcvs_survey_time($order_id){
+	if( get_current_blog_id() == 1 ){
+		wp_redirect(network_site_url('/warehouse-evaluation'));
+	} elseif( persona_shopping_complete(1) && persona_shopping_complete(1) ){
+		wp_redirect(network_site_url('/shopping-evaluation?store_id='.get_current_blog_id()));
+	} else {
+		wp_redirect(network_site_url('/final-evaluation'));
+	}
+}
+
+add_action( 'gform_after_submission', 'dcvs_redirect_to_dashboard_after_evaluation' );
+function dcvs_redirect_to_dashboard_after_evaluation($entry){
+	wp_redirect('matts/dashboard');
+}
+
+add_filter( 'gform_field_value_user_id', 'dcvs_add_user_id_to_form' );
+function dcvs_add_user_id_to_form( $value ) {
+	return get_current_user_id();
+}
