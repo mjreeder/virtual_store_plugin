9feb14664e9882fe33d8ae2cc8ccb852f79d808e
Richard Downey <rdowney117@gmail.com>
2017-04-20 00:01:44 -0400

    Format toast.js code

--- js/toast.js
+++ js/toast.js
@@ -1,6 +1,9 @@
 function displayToast() {
     var toast = document.getElementById("toast");
     toast.className = toast.className + " show";
-    setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
+    setTimeout( function () {
+        toast.className = toast.className.replace("show", "");
+    }, 3000);
 }
+
 displayToast();
