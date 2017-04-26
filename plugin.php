7040260509beb3d12aaf0e94d30007aee323fe36
Richard Downey <rdowney117@gmail.com>
2017-04-26 09:40:43 -0400

    Add logic to ensure user is logged in

--- plugin.php
+++ plugin.php
@@ -315,3 +315,25 @@ function dcvs_enqueue_admin_script()
         wp_enqueue_style( 'dcvs_product_edit_style', plugins_url( '/assets/css/editProduct.css', __FILE__ ) );
     }
 }
+
+//https://trickspanda.com/force-users-login-viewing-wordpress/
+
+function dcvs_getUrl() {
+    $url  = isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ? 'https' : 'http';
+    $url .= '://' . $_SERVER['SERVER_NAME'];
+    $url .= in_array( $_SERVER['SERVER_PORT'], array('80', '443') ) ? '' : ':' . $_SERVER['SERVER_PORT'];
+    $url .= $_SERVER['REQUEST_URI'];
+    return $url;
+}
+function dcvs_forcelogin() {
+    if( !is_user_logged_in() ) {
+        $url = dcvs_getUrl();
+        $whitelist = apply_filters('v_forcelogin_whitelist', array());
+        $redirect_url = apply_filters('v_forcelogin_redirect', $url);
+        if( preg_replace('/\?.*/', '', $url) != preg_replace('/\?.*/', '', wp_login_url()) && !in_array($url, $whitelist) ) {
+            wp_safe_redirect( wp_login_url( $redirect_url ), 302 ); exit();
+        }
+    }
+}
+add_action('init', 'dcvs_forcelogin');
+
