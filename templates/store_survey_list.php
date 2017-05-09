d3a58deb4fb0c640f2db7c496d81a368a38ada23
Riley Paulsen <rileypaulsen.bsu@gmail.com>
2017-05-09 10:18:02 -0400

    Adds store feedback? Will test live……….

--- templates/store_survey_list.php
+++ templates/store_survey_list.php
@@ -11,6 +11,25 @@ if(!filter_var($current_user_id, FILTER_VALIDATE_INT) || $current_user_id == 0){
     wp_die("invalid logged in user");
 }
 
+$shopping_evaluation_id = 2;
+$storeFieldIdentifier = 12;
+
+$form = GFAPI::get_form($shopping_evaluation_id);
+
+$args = array(
+	'field_filters' => array(
+		array(
+			'key' => 'created_by',
+			'value' => $current_user_id
+		),
+		array(
+			'key' => $storeFieldIdentifier,
+			'value' => DCVS_Store_Management::get_store_by_user($current_user_id)
+		)
+	)
+);
+$entries = GFAPI::get_entries($shopping_evaluation_id, $args);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,41 +51,19 @@ if(!filter_var($current_user_id, FILTER_VALIDATE_INT) || $current_user_id == 0){
   <h1 class="title">Store Feedback</h1>
   <a href="<?php echo dcvs_get_landing_page_url(); ?>" class="backButton"><p>Back to Dashboard</p></a>
   <div class="entries">
-  			<ul>
-  									<li id="#entry10">
-  						<h2>2017-05-03 13:09:59</h2>
-  						<dl>
-  															<dt>Was it Fun?</dt>
-  								<dd><ul><li>Aw hell no</li></ul></dd>
-  															<dt>What was the most exciting part?</dt>
-  								<dd><ul><li>The Fashion</li><li>The Drawings</li></ul></dd>
-  															<dt>User ID</dt>
-  								<dd><ul><li>72</li></ul></dd>
-  													</dl>
-  					</li>
-  									<li id="#entry9">
-  						<h2>2017-05-03 13:09:55</h2>
-  						<dl>
-  															<dt>Was it Fun?</dt>
-  								<dd><ul><li>Yes</li></ul></dd>
-  															<dt>What was the most exciting part?</dt>
-  								<dd><ul><li>The Fashion</li></ul></dd>
-  															<dt>User ID</dt>
-  								<dd><ul><li>72</li></ul></dd>
-  													</dl>
-  					</li>
-  									<li id="#entry8">
-  						<h2>2017-05-03 13:09:50</h2>
-  						<dl>
-  															<dt>Was it Fun?</dt>
-  								<dd><ul><li>Yes</li></ul></dd>
-  															<dt>What was the most exciting part?</dt>
-  								<dd><ul><li>The Fashion</li></ul></dd>
-  															<dt>User ID</dt>
-  								<dd><ul><li>72</li></ul></dd>
-  													</dl>
-  					</li>
-  							</ul>
-  		</div>
+		<ul>
+			<?php foreach($entries as $entry): ksort($entry); ?>
+				<li id="#entry<?= $entry['id']; ?>">
+					<h2><?= $entry['date_created']; ?></h2>
+					<dl>
+						<?php foreach($form['fields'] as $question): ?>
+							<dt><?= $question->label; ?></dt>
+							<dd><?= dcvs_get_answers_based_on_question_id($entry, (string) $question->id); ?></dd>
+						<?php endforeach; ?>
+					</dl>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
 </body>
 </html>
