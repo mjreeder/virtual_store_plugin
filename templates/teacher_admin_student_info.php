59b3b14765389a94ea8273fb50bb5a969ba28c32
Corey Halbert <cjhalbert@bsu.edu>
2017-05-11 14:25:05 -0400

    set autocomplete off on search bar and fix budget bar style override

--- templates/teacher_admin_student_info.php
+++ templates/teacher_admin_student_info.php
@@ -3,7 +3,7 @@
 	<aside class="studentList">
 		<div class="searchBar">
 			<img src="<?php echo plugins_url( 'assets/images/search.svg', dirname(__FILE__)); ?>" rel="stylesheet" alt="">
-			<input type="search" id='search' placeholder="search" oninput="studentSearch()">
+			<input type="search" id='search' placeholder="search" oninput="studentSearch()" autocomplete="off">
 		</div>
 		<ul id="students">
 			<?php display_student_list(); ?>
