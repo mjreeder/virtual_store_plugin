44bd6fa8d166c874429e8f50dc7b96c9a8056ef9
Josh Schoen <jmschoen42@gmail.com>
2017-05-18 15:30:11 -0400

    update style

--- templates/teacher_admin_student_info.php
+++ templates/teacher_admin_student_info.php
@@ -162,7 +162,7 @@ function display_current_student_info()
 				<div class="">
 					<a href="<?php echo get_site_url() . '/wp-admin/admin.php?page=dcvs_teacher&student_id=' . $currentDisplayStudent . '&section=surveys&form_id=' . $warehouse_evaluation_id; ?>"
 					   class="button">WAREHOUSE SURVEYS</a>
-					<a href="<?php echo plugins_url( 'templates/store_survey_list.php', dirname(__FILE__)) ?>"><button class="button btnStore">STORE FEEDBACK</button></a>
+					<a href="<?php echo plugins_url( 'templates/store_survey_list.php', dirname(__FILE__)) ?>"><button class="button">STORE FEEDBACK</button></a>
 				</div>
 
 				<!-- TODO get remaining budget-->
