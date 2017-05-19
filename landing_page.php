02e9764a6cdfd8fad582383a8d0289e77bd971c5
Riley Paulsen <rileypaulsen.bsu@gmail.com>
2017-05-19 12:44:07 -0400

    Fixes video display in the student dashboard Adds video instructions Adds AJAX requests for keeping track of the current user’s progress in the site

--- landing_page.php
+++ landing_page.php
@@ -13,3 +13,46 @@ function dcvs_landing_page_content($content){
     }
     return $content;
 }
+
+function dcvs_get_videos(){
+	$opts = array(
+		'http'=>array(
+			'method'=>"GET",
+			'header'=>"Authorization: Bearer 2f31f4053bb21a971bad92c108b253bf"
+		)
+	);
+
+	$context = stream_context_create($opts);
+
+	// Open the file using the HTTP headers set above
+	$file = json_decode(file_get_contents('https://api.vimeo.com/users/10466342/albums/4481462/videos', false, $context), $assoc_array = false );
+
+	return $file->data;
+}
+
+function dcvs_get_video_js(){
+	$videos = dcvs_get_videos();
+	$data = array_map(function($video){
+		$id = preg_replace('/\D+/','', $video->uri);
+		return [
+			'id'=>$id,
+			'title'=>$video->name,
+			'description'=>$video->description,
+			'duration'=>gmdate("i:s", $video->duration),
+			'embed'=>$video->embed->html
+		];
+	}, $videos);
+	return $data;
+}
+
+function dcvs_get_video_progress_for_user(){
+	get_user_meta(get_current_user_id(), 'video_progress', true);
+}
+
+function dcvs_update_video_progress_for_user(){
+	$videoID = filter_var($_POST['data']['videoID'], FILTER_SANITIZE_NUMBER_INT);
+	update_user_meta(get_current_user_id(), 'video_progress', $videoID);
+	die('');
+}
+
+add_action('wp_ajax_video_progress', 'dcvs_update_video_progress_for_user');
